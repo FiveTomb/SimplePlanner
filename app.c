78e0df7baeed611f5b202e924caff4cce2963c98
FiveTomb <alexgames4160@gmail.com>
2023-12-30 15:13:00 -0500

    Changes to how user input is handled

--- app.c
+++ app.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INITIAL_SIZE 5
-
 // Defining what a "Task" contains
 typedef struct {
     int id;
@@ -18,15 +16,13 @@ typedef struct {
 // Function prototypes
 void welcomeMessage(void);
 void printTaskDetails(Task task);
+void readString(char[]);
 Task createTask();
 
 int main(int argc, char *argv[])
 {
-    createTask();
-    /*
-    welcomeMessage();
-    scanf("%d", &choice);
-    */
+    Task myTask = createTask();
+    printTaskDetails(myTask);
 
     return 0;
 }
@@ -43,30 +39,65 @@ void welcomeMessage()
 
 void printTaskDetails(Task task)
 {
-    printf("Task ID: %d\n", task.id);
+    //printf("Task ID: %d\n", task.id);
     printf("Task Name: %s\n", task.name);
     printf("Description: %s\n", task.description);
     printf("Due Date: %s\n", task.dueDate);
+    /*
     printf("Priority: %d\n", task.priority);
     printf("Completed: %d\n", task.completed);
     printf("\n");
+    */
 }
 
 // Function that takes user input and creates a new task to add to the list
 Task createTask()
 {
-    int id, priority;
+    // int b is user input to a boolean
+    int id, priority, b;
     char name[100], description[256], dueDate[11];
     bool completed;
     Task newTask;
 
+    printf("Enter a name for this task: ");
+    if (fgets(name, sizeof(name), stdin) == NULL)
+    {
+        printf("Failed to read input stream");
+    }
+    else {
+        name[strcspn(name, "\n")] = '\0';
+        strcpy(newTask.name, name);
+    }
+    printf("Enter a description for this task: ");
+    if (fgets(description, sizeof(description), stdin) == NULL)
+        printf("Failed to read input stream");
+    else
+    {
+        description[strcspn(description, "\n")] = '\0';
+        strcpy(newTask.description, description);
+    }
+    printf("Enter a deadline for this task: ");
+    if (fgets(dueDate, sizeof(dueDate), stdin) == NULL)
+        printf("Failed to read input stream");
+    else
+    {
+        dueDate[strcspn(dueDate, "\n")] = '\0';
+        strcpy(newTask.dueDate, dueDate);
+    }
+    /*
+    printf("Enter the task ID: ");
     scanf("%d", &id);
     newTask.id = id;
-    strcpy(newTask.name, name);
-    strcpy(newTask.description, description);
-    strcpy(newTask.dueDate, dueDate);
+    printf("Enter the task priority: ");
+    scanf("%d", &priority);
     newTask.priority = priority;
+    printf("Has the task been completed? (0 for no 1 for yes): ");
+    scanf("%d", &b);
+    if (b)
+        completed = true;
+    else
+        completed = false;
     newTask.completed = completed;
-
+    */
     return newTask;
 }
